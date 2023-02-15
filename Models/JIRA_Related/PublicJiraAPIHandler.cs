80c91877e9ea3f2bfc67ece309d1148e01b468e2
Peiyuan Wu <peiyuan.wu@syntecclub.com.tw>
2023-02-15 08:22:38 +0000

    20230215 ICT-12967_危機處理專案BPM資料串接JIRA 會議結論 1.新增可更動當責單位內容 2.議題連結建立流程同步BPM欄位
    git-svn-id: http://svn.syntecclub.com.tw:815/svn/ICT/API/ITAPI/branch/0215_debug_API_wu@133649 8affe271-0f23-1740-abfe-5752154cf714

--- Models/JIRA_Related/PublicJiraAPIHandler.cs
+++ Models/JIRA_Related/PublicJiraAPIHandler.cs
@@ -24,20 +24,31 @@ namespace SyntecITWebAPI.Models.JiraAPI_Related
 		internal string CreateJiraIssueLinks( JiraIssueLinkCreate JiraIssueLinkCreateParameter )
 		{
 			HttpClient client = new HttpClient();
-			string results="";
+			client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue( "Basic", "aXNzdWVyb2JvdDpTeW50ZWMxMjM0" );
+			string results = "";
+
+			//取得Issue各欄位
+				string targetUrl = "https://jira.syntecclub.com/rest/api/2/issue/" + JiraIssueLinkCreateParameter.issueID;
+			HttpResponseMessage Response = client.GetAsync( targetUrl ).Result;
+			JObject jobjectRI = JObject.Parse( Response.Content.ReadAsStringAsync().Result );
+
+			for( int j = 0; j < jobjectRI[ "fields" ][ "issuelinks" ].Count(); j++ )
+			{
+				HttpResponseMessage DeleteResponse = client.DeleteAsync( "https://jira.syntecclub.com/rest/api/2/issueLink/" + jobjectRI[ "fields" ][ "issuelinks" ][ j ][ "id" ].ToString() ).Result;
+				results = DeleteResponse.Content.ReadAsStringAsync().Result;
+			}
 
 			foreach( var outwardIssue in JiraIssueLinkCreateParameter.RelatedIssues.Split( ',' ) )
-			{			
+			{
 				//
 				HttpContent HContent = new StringContent( "{\"inwardIssue\": {\"key\": \"" + JiraIssueLinkCreateParameter.issueID + "\"},\"outwardIssue\": {\"key\": \"" + outwardIssue + "\"},\"type\": {\"id\": \"" + JiraIssueLinkCreateParameter.linkType + "\"}}", Encoding.UTF8, "application/json" );
 
-				string targetUrl = "https://jira.syntecclub.com/rest/api/2/issueLink";
-				client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue( "Basic", "aXNzdWVyb2JvdDpTeW50ZWMxMjM0" );
+				targetUrl = "https://jira.syntecclub.com/rest/api/2/issueLink";
 				HttpResponseMessage insertLinkResponse = client.PostAsync( targetUrl, HContent ).Result;
 				results = insertLinkResponse.Content.ReadAsStringAsync().Result;
 			}
-			
-			return results;
+
+			return results.ToString();
 		}
 
 
@@ -45,7 +56,7 @@ namespace SyntecITWebAPI.Models.JiraAPI_Related
 		internal string UpdateJiraIssueStatu( JiraIssueTransition JiraIssueTransitionParameter )
 		{
 			HttpClient client = new HttpClient();
-		
+
 			//變更議題狀態
 			HttpContent issueTransitionHContent = new StringContent( "{\"transition\": {" +
 				"\"id\":\"" + JiraIssueTransitionParameter.transitionID + "\"" + //工作流程id
@@ -63,27 +74,27 @@ namespace SyntecITWebAPI.Models.JiraAPI_Related
 			HttpClient client = new HttpClient();
 			HttpContent HContent = new StringContent( "{\"fields\": {" +
 					"\"project\": {\"id\": \"15590\" }," + //危機處理測試專區 project id:15590；危機處理專區 project id:15390
-					"\"reporter\": {\"name\": \"" + CreateJiraIssueParameter.reporter+ "\" }," +  //報告人
+					"\"reporter\": {\"name\": \"" + CreateJiraIssueParameter.reporter + "\" }," +  //報告人
 					"\"customfield_16821\":  {\"value\":\"" + CreateJiraIssueParameter.createDept + "\"}," +  //發起單位
 					"\"summary\": \"" + CreateJiraIssueParameter.summary + "\"," + //摘要
 					"\"description\": \"" + CreateJiraIssueParameter.description + "\"," + //描述
-					"\"assignee\": {\"name\": \"" + CreateJiraIssueParameter.assignee+ "\" }," + //負責人
+					"\"assignee\": {\"name\": \"" + CreateJiraIssueParameter.assignee + "\" }," + //負責人
 					"\"duedate\": \"" + CreateJiraIssueParameter.duedate + "\"," + //到期日
 					"\"issuetype\": {\"name\": \"故事\" }," + //類型
 					"\"priority\": {\"id\": \"1\" }," + //嚴重程度
 					"\"components\": [{\"name\": \"危機處理\" }]" + //模組
 				"}}", Encoding.UTF8, "application/json" );
-			
-			string targetUrl = "https://jira.syntecclub.com/rest/api/2/issue";			
+
+			string targetUrl = "https://jira.syntecclub.com/rest/api/2/issue";
 			client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue( "Basic", "aXNzdWVyb2JvdDpTeW50ZWMxMjM0" );
 
 			HttpResponseMessage response = client.PostAsync( targetUrl, HContent ).Result;
 
 			//JIRA議題監看者設定為報告人
 			var JResult = JObject.Parse( response.Content.ReadAsStringAsync().Result.ToString() );
-			HttpContent AddWatcherHContent = new StringContent( "\"" + CreateJiraIssueParameter.reporter + "\"" , Encoding.UTF8, "application/json" );
+			HttpContent AddWatcherHContent = new StringContent( "\"" + CreateJiraIssueParameter.reporter + "\"", Encoding.UTF8, "application/json" );
 
-			targetUrl = "https://jira.syntecclub.com/rest/api/2/issue/"+ JResult[ "key" ].ToString() + "/watchers";
+			targetUrl = "https://jira.syntecclub.com/rest/api/2/issue/" + JResult[ "key" ].ToString() + "/watchers";
 			client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue( "Basic", "aXNzdWVyb2JvdDpTeW50ZWMxMjM0" );
 
 			HttpResponseMessage AddWatcherResponse = client.PostAsync( targetUrl, AddWatcherHContent ).Result;
@@ -100,24 +111,26 @@ namespace SyntecITWebAPI.Models.JiraAPI_Related
 			HttpContent HContent = new StringContent( "{\"fields\": {" +
 					"\"assignee\": {\"name\": \"" + EditJiraIssueParameter.teamLeader + "\" }," + //負責人
 					"\"duedate\": \"" + EditJiraIssueParameter.duedate + "\"," + //到期日
+					"\"customfield_16827\": \"" + EditJiraIssueParameter.teamLeaderDept + "\"," + //當責單位
 					"\"customfield_17021\": \"" + EditJiraIssueParameter.teamMembers + "\"" + //危機小組成員
 				"}}", Encoding.UTF8, "application/json" );
 			string targetUrl = "https://jira.syntecclub.com/rest/api/2/issue/" + EditJiraIssueParameter.issueID;
 			client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue( "Basic", "aXNzdWVyb2JvdDpTeW50ZWMxMjM0" );
 
 			HttpResponseMessage response = client.PutAsync( targetUrl, HContent ).Result;
-			
-			return response.ToString();			
+
+			return response.ToString();
 		}
 
 		//短期對策擬定
 		internal string EditJiraIssueForShortTermPlan( EditJiraIssue EditJiraIssueParameter )
-		{			
+		{
 			HttpClient client = new HttpClient();
 			//變更議題欄位內容
 			HttpContent HContent = new StringContent( "{\"fields\": {" +
 					"\"assignee\": {\"name\": \"" + EditJiraIssueParameter.teamLeader + "\" }," + //負責人
 					"\"customfield_17027\": \"" + EditJiraIssueParameter.teamLeaderDept + "\"," + //短期對策處理單位
+					"\"customfield_16827\": \"" + EditJiraIssueParameter.teamLeaderDept + "\"," + //當責單位
 					"\"customfield_17023\": \"" + EditJiraIssueParameter.problemAnalysis + "\"," + //問題標定
 					"\"customfield_16829\": \"" + EditJiraIssueParameter.shortTermPlan + "\"," + //短期對策策劃
 					"\"customfield_16837\": \"" + EditJiraIssueParameter.shortTermDueDate + "\"" + //短期對策預計完成日
@@ -133,7 +146,7 @@ namespace SyntecITWebAPI.Models.JiraAPI_Related
 		//短期對策執行
 		internal string EditJiraIssueForShortTermExecute( EditJiraIssue EditJiraIssueParameter )
 		{
-		var client = new HttpClient();
+			var client = new HttpClient();
 			//變更議題欄位內容
 			HttpContent HContent = new StringContent( "{\"fields\": {" +
 					"\"duedate\": \"" + EditJiraIssueParameter.duedate + "\"," + //到期日
@@ -151,11 +164,12 @@ namespace SyntecITWebAPI.Models.JiraAPI_Related
 		//長期對策擬定
 		internal string EditJiraIssueForLongTermPlan( EditJiraIssue EditJiraIssueParameter )
 		{
-		var client = new HttpClient();
+			var client = new HttpClient();
 			//變更議題欄位內容
 			HttpContent HContent = new StringContent( "{\"fields\": {" +
 					"\"assignee\": {\"name\": \"" + EditJiraIssueParameter.teamLeader + "\" }," + //負責人
 					"\"customfield_17028\": \"" + EditJiraIssueParameter.teamLeaderDept + "\"," + //長期對策處理單位
+					"\"customfield_16827\": \"" + EditJiraIssueParameter.teamLeaderDept + "\"," + //當責單位
 					"\"customfield_17026\": \"" + EditJiraIssueParameter.problemAnalysis + "\"," + //真因分析
 					"\"customfield_16830\": \"" + EditJiraIssueParameter.longTermPlan + "\"," + //長期對策策劃
 					"\"customfield_16840\": \"" + EditJiraIssueParameter.longTermDueDate + "\"," + //長期對策預計完成日
@@ -172,7 +186,7 @@ namespace SyntecITWebAPI.Models.JiraAPI_Related
 		//長期對策執行
 		internal string EditJiraIssueForLongTermExecute( EditJiraIssue EditJiraIssueParameter )
 		{
-		var client = new HttpClient();
+			var client = new HttpClient();
 			//變更議題欄位內容
 			HttpContent HContent = new StringContent( "{\"fields\": {" +
 					"\"duedate\": \"" + EditJiraIssueParameter.duedate + "\"," + //到期日
@@ -191,7 +205,7 @@ namespace SyntecITWebAPI.Models.JiraAPI_Related
 		//歸檔
 		internal string CloseJiraIssue( CloseJiraIssue CloseJiraIssueParameter )
 		{
-		var client = new HttpClient();
+			var client = new HttpClient();
 			//變更議題欄位內容
 			HttpContent HContent = new StringContent( "{\"fields\": {" +
 					"\"assignee\": {\"name\": \"" + CloseJiraIssueParameter.assignee + "\" }," + //負責人
