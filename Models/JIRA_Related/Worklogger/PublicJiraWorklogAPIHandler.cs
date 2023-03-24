f289f473da400d669489305b4746cb37a06fa981
Peiyuan Wu <peiyuan.wu@syntecclub.com.tw>
2023-03-24 09:48:15 +0000

    git-svn-id: http://svn.syntecclub.com.tw:815/svn/ICT/API/ITAPI/branch/0323_JIRA_Worklog_API_wu@135437 8affe271-0f23-1740-abfe-5752154cf714

--- Models/JIRA_Related/Worklogger/PublicJiraWorklogAPIHandler.cs
+++ Models/JIRA_Related/Worklogger/PublicJiraWorklogAPIHandler.cs
@@ -1,5 +1,6 @@
 ﻿using Newtonsoft.Json.Linq;
 using SyntecITWebAPI.ParameterModels.JIRA_Related.Worklogger;
+using SyntecITWebAPI.Common.DBRelated.DBManagers.JIRA_Related;
 using System;
 using System.Collections.Generic;
 using System.Data;
@@ -66,6 +67,95 @@ namespace SyntecITWebAPI.Models.JiraAPI_Related.Worklogger
 			return Allissuekey;
 		}
 
+		internal bool UpsertJiraWorkLogRelatedIssue( UpsertJiraWorkLogRelatedIssue UpsertJiraWorkLogRelatedIssueParameter )
+		{
+
+			HttpClient client = new HttpClient();
+
+			string targetUrl = "https://jira.syntecclub.com/rest/api/2/issue/" + UpsertJiraWorkLogRelatedIssueParameter.issueID;
+			//Basic Authentication
+			client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue( "Basic", "aXNzdWVyb2JvdDpTeW50ZWMxMjM0" );
+
+			HttpResponseMessage response = client.GetAsync( targetUrl).Result;
+
+			JObject jobjectRI = JObject.Parse( response.Content.ReadAsStringAsync().Result );
+
+
+			UpsertJiraWorkLogRelatedIssueParameter.summary = jobjectRI[ "fields" ][ "summary" ].ToString();
+
+
+			UpsertJiraWorkLogRelatedIssueParameter.type = jobjectRI[ "fields" ][ "issuetype" ][ "name" ].ToString();
+
+
+			try
+			{
+				foreach( var component in jobjectRI[ "fields" ][ "components" ] )
+				{
+					UpsertJiraWorkLogRelatedIssueParameter.components = UpsertJiraWorkLogRelatedIssueParameter.components  + component[ "name" ]+ ",";
+				}
+			}
+			catch
+			{
+				UpsertJiraWorkLogRelatedIssueParameter.components = "";
+			}
+
+			try
+			{
+				foreach( var label in jobjectRI[ "fields" ][ "labels" ] )
+				{
+					UpsertJiraWorkLogRelatedIssueParameter.labels = UpsertJiraWorkLogRelatedIssueParameter.labels + label.ToString() + ",";
+				}				
+			}
+			catch
+			{
+				UpsertJiraWorkLogRelatedIssueParameter.labels = "";
+			}
+
+			try
+			{
+				UpsertJiraWorkLogRelatedIssueParameter.assignee = jobjectRI[ "fields" ][ "assignee" ][ "name" ].ToString();
+			}
+			catch
+			{
+				UpsertJiraWorkLogRelatedIssueParameter.assignee = "";
+			}
+
+			try
+			{
+				UpsertJiraWorkLogRelatedIssueParameter.duedate = jobjectRI[ "fields" ][ "duedate" ].ToString();
+			}
+			catch
+			{
+				UpsertJiraWorkLogRelatedIssueParameter.duedate = "";
+			}
+
+
+
+			try
+			{
+				if( jobjectRI[ "fields" ][ "timetracking" ][ "originalEstimate" ] != null )
+				{
+				UpsertJiraWorkLogRelatedIssueParameter.originalEstimate = jobjectRI[ "fields" ][ "timetracking" ][ "originalEstimate" ].ToString();
+				}
+			}
+			catch
+			{
+				UpsertJiraWorkLogRelatedIssueParameter.originalEstimate = "";
+			}
+
+			bool bResult = m_WorkloggerDBManager.UpsertJiraWorkLogRelatedIssue( UpsertJiraWorkLogRelatedIssueParameter );
+
+			
+			return bResult;
+		}
+
+		internal bool InsertWorkLogs( InsertWorkLogs InsertWorkLogsParameter )
+		{
+			bool bResult = m_WorkloggerDBManager.InsertWorkLogs( InsertWorkLogsParameter );
+
+
+			return bResult;
+		}
 
 		//新增JIRA議題工作日誌
 		internal JObject AddWorklog( JiraWorkLog JiraWorkLogParameter )
@@ -83,18 +173,49 @@ namespace SyntecITWebAPI.Models.JiraAPI_Related.Worklogger
 
 			JObject jobjectRI = JObject.Parse( response.Content.ReadAsStringAsync().Result );
 
-			//for( int j = 0; j < jobjectRI[ "fields" ][ "issuelinks" ].Count(); j++ )
-			//{
-			//}
-
 			return jobjectRI;
 		}
 
+		//新增JIRA專案到DB
+		internal bool InsertJiraProjects( InsertJiraProjects InsertJiraProjectsParameter )
+		{
+			HttpClient client = new HttpClient();
 
+			string targetUrl = "https://jira.syntecclub.com/rest/api/2/project";
+			//Basic Authentication
+			client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue( "Basic", "aXNzdWVyb2JvdDpTeW50ZWMxMjM0" );
+
+			HttpResponseMessage response = client.GetAsync( targetUrl ).Result;
+
+			bool bResult = false;
+			foreach( var project in response.Content.ReadAsStringAsync().Result.Replace( "[{", "" ).Replace( "}]", "" ).Split( "},{" ) )
+			{
+				JObject jobjectRI = JObject.Parse( "{"+project+"}" );
+
+				InsertJiraProjectsParameter.projectID = jobjectRI[ "id" ].ToString();
+				InsertJiraProjectsParameter.projectKey = jobjectRI[ "key" ].ToString();
+				InsertJiraProjectsParameter.projectName = jobjectRI[ "name" ].ToString();
+				InsertJiraProjectsParameter.projectTypeKey = jobjectRI[ "projectTypeKey" ].ToString();
+				bResult = m_WorkloggerDBManager.InsertJiraProjects( InsertJiraProjectsParameter );
+			}
+			return bResult;
+		}
+		//新增各專案報工tag到DB
+		internal bool InsertProjectTag( InsertProjectTag InsertProjectTagParameter )
+		{
+			bool bResult = m_WorkloggerDBManager.InsertProjectTag( InsertProjectTagParameter );
+			return bResult;
+		}
+		internal bool DeleteProjectTag( DeleteProjectTag DeleteProjectTagParameter )
+		{
+			bool bResult = m_WorkloggerDBManager.DeleteProjectTag( DeleteProjectTagParameter );
+			return bResult;
+		}
 		#endregion Internal Methods
 
 		#region Private Fields
 
+		private WorkloggerDBManager m_WorkloggerDBManager = new WorkloggerDBManager();
 
 		#endregion Private Fields
 	}
--- ParameterModels/JIRA_Related/Worklogger/JiraWorklogAPIParameter.cs
+++ ParameterModels/JIRA_Related/Worklogger/JiraWorklogAPIParameter.cs
@@ -1,7 +1,7 @@
 ﻿
 namespace SyntecITWebAPI.ParameterModels.JIRA_Related.Worklogger
 {
-	
+
 	public class JiraWorkLog
 	{
 		public string issueID
@@ -36,7 +36,150 @@ namespace SyntecITWebAPI.ParameterModels.JIRA_Related.Worklogger
 		{
 			get; set;
 		}
+		public string JsessionID
+		{
+			get; set;
+		}
+	}
+
+	public class JiraWorkLogRelatedIssueAllField
+	{
+		public string issueID
+		{
+			get; set;
+		}
+		public string summary
+		{
+			get; set;
+		}
+		public string type
+		{
+			get; set;
+		}
+		public string components
+		{
+			get; set;
+		}
+		public string labels
+		{
+			get; set;
+		}
+		public string assignee
+		{
+			get; set;
+		}
+		public string duedate
+		{
+			get; set;
+		}
+		public string originalEstimate
+		{
+			get; set;
+		}
+		public string BasicAuth
+		{
+			get; set;
+		}
+
+		
+	}
+
+	public class UpsertJiraWorkLogRelatedIssue : JiraWorkLogRelatedIssueAllField
+	{
+
+	}
+
+
+	public class JiraWorkLogsAllField
+	{
+		public string issueID
+		{
+			get; set;
+		}
+		public string empID
+		{
+			get; set;
+		}
+		public string workLogID
+		{
+			get; set;
+		}
+		public string timeSpentSeconds
+		{
+			get; set;
+		}
+		public string created
+		{
+			get; set;
+		}
+		public string started
+		{
+			get; set;
+		}
+		public string type
+		{
+			get; set;
+		}
+		public string tags
+		{
+			get; set;
+		}
+		public string comment
+		{
+			get; set;
+		}
+	}
+
+	public class InsertWorkLogs : JiraWorkLogsAllField
+	{
+
+	}
+
+	public class JiraProjectTagsAllField
+	{
+		public int No
+		{
+			get; set;
+		}
+		public string projectID
+		{
+			get; set;
+		}
+		public string projectKey
+		{
+			get; set;
+		}
+		public string projectName
+		{
+			get; set;
+		}
+		public string projectTypeKey
+		{
+			get; set;
+		}
+		public string tagName
+		{
+			get; set;
+		}
+		public string SuperDeptName
+		{
+			get; set;
+		}
 	}
 	
 
+	public class InsertJiraProjects : JiraProjectTagsAllField
+	{
+
+	}
+
+	public class InsertProjectTag : JiraProjectTagsAllField
+	{
+
+	}
+
+	public class DeleteProjectTag : JiraProjectTagsAllField
+	{
+
+	}
 }
